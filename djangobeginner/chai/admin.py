6664274cdd6335b97d4aaec7fcbb9317ef973c96
Dipesh <dipeshpatro@gmail.com>
2025-07-28 19:41:44 +0530

    as

--- djangobeginner/chai/admin.py
+++ djangobeginner/chai/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Chaivariety
+from .models import Chaivariety, ChaiReview, Store, ChaiCertificate
 
 # Register your models here.
+
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 1 
+
+
+
 admin.site.register(Chaivariety)
--- djangobeginner/chai/models.py
+++ djangobeginner/chai/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Chaivariety(models.Model):
@@ -21,3 +22,39 @@ class Chaivariety(models.Model):
 
     def __str__(self):
         return self.name
+    
+    # one to many
+
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(Chaivariety, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+    comment = models.TextField(default="")
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.chai.name} Review"
+    
+
+# many to many 
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=255)
+    chai_varieties = models.ManyToManyField(Chaivariety, related_name='stores')
+
+    def __str__(self):
+        return self.name
+    
+#one to one 
+
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(Chaivariety, on_delete= models.CASCADE, related_name='Certificate')
+    certificate_number = models.CharField(max_length=100)
+    issued_date = models.DateTimeField(default=timezone.now)
+    valid_until = models.DateTimeField()
+
+    def __str__(self):
+        return f' certificate for {self.chai.name}'
+    
+    
